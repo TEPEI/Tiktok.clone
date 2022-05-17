f2f00742848b61ade3b5ee5a6f68aaa4c2388ab8
TEPEI <teppei3877@gmail.com>
2022-05-17 15:27:48 +0900

    Initial commit

---
+++ app/src/main/java/tiktok/clone/app/MainActivity.java
@@ -0,0 +1,14 @@
+package tiktok.clone.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
